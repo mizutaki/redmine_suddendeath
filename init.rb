960cfa1d6d3cb19b2e93e08a551ed0be7e82590b
mizutaki <mizutaki4@gmail.com>
2016-04-22 22:07:22 +0000

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :suddendeath do
+  name 'Suddendeath plugin'
+  author 'mizutaki'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/mizutaki/redmine_suddendeath'
+  author_url 'http://example.com/about'
+end
