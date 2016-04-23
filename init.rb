c8393bdf4419a99e2abab594a4bf352d43cb3bf5
mizutaki <mizutaki4@gmail.com>
2016-04-23 12:37:53 +0000

    syakyou

--- init.rb
+++ init.rb
@@ -6,3 +6,16 @@ Redmine::Plugin.register :suddendeath do
   url 'https://github.com/mizutaki/redmine_suddendeath'
   author_url 'http://example.com/about'
 end
+
+Redmine::WikiFormatting::Macros.register do
+  desc "string decoration suddendeath"
+  macro :sd do |obj, args|
+    if (args.size != 1)
+      raise "string decoration macro is given only argument."
+    else
+      word = args.first.strip
+      addr = "http://ja.wikipedia.org/wiki/" + CGI.escape(word)
+      return link_to(word, addr, :class => "external")
+    end
+  end
+end
