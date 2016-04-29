388b830bf4b33574dca61dd19c09aded46c625cd
mizutaki <mizutaki4@gmail.com>
2016-04-29 13:54:40 +0000

    add new macro suddendeath

--- init.rb
+++ init.rb
@@ -13,9 +13,19 @@ Redmine::WikiFormatting::Macros.register do
     if (args.size != 1)
       raise "string decoration macro is given only argument."
     else
-      word = args.first.strip
-      addr = "http://ja.wikipedia.org/wiki/" + CGI.escape(word)
-      return link_to(word, addr, :class => "external")
+      str = args.first.strip
+      org_strmax = str.each_line.map {|line| line.chomp.length}.max + 2
+      middle = str.each_line.map{|line| line.chomp.insert(0, "＞　")}.join("\n")
+      middle = middle.each_line.map{|line| line.chomp.insert(org_strmax, "　＜")}.join("\n")
+      middle_strmax = middle.each_line.map{|line| line.chomp.length}.max
+      top = "人" * org_strmax
+      bottom = "Y^" * org_strmax
+decoration_str = <<"EOS"
+#{top}
+#{middle}
+#{bottom}
+EOS
+      textilizable(decoration_str, {:object=>nil, :attachments=>[]})
     end
   end
 end
